57960d7e9aca90acffddd03528c270584b6dd53e
jessemusic <jmc_cursos@hotmail.com>
2021-03-17 16:55:58 -0300

    utilizando templates e método de dinamitização do html, css javascript

--- programação js na web com DOM/DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
+++ programação js na web com DOM/DOM-projeto_incial/projeto_inicial/listaDeTarefas.js
@@ -5,13 +5,29 @@ const criarTarefa = (evento) => {
 
 
 
-    const tarefa = document.querySelector('[data-task]')
+    const list = document.querySelector('[data-list]')
+
+    const tarefa = document.createElement('li')
+    tarefa.classList.add('task')
     const conteudo = `<p class="content">${valor}</p>`
 
     tarefa.innerHTML = conteudo
-    input.value = " ";
+    tarefa.appendChild(BotaoConclui())
+    list.appendChild(tarefa)
+    input.value = " "
 }
 
 const novaTarefa = document.querySelector('[data-form-button]');
 
 novaTarefa.addEventListener('click', criarTarefa)
+
+
+const BotaoConclui = () => {
+    const botaoConclui = document.createElement('button')
+    botaoConclui.classList.add('check-button')
+    botaoConclui.innerText = 'concluir'
+    botaoConclui.addEventListener('click', () => {
+        console.log('fui clicado')
+    })
+    return botaoConclui
+}
